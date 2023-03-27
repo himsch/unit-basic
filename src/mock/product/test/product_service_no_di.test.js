9513ef88ee93e1ed9d809850077f142447e3728f
song <55rsearchco@gmail.com>
2023-03-27 15:03:27 +0900

    Mock을 이용한 테스트

---
+++ src/mock/product/test/product_service_no_di.test.js
@@ -0,0 +1,31 @@
+const ProductService = require('../product_service_no_di');
+const ProductClient = require('../product_client');
+jest.mock('../product_client');
+
+describe('ProductService', () => {
+  const fetchItems = jest.fn(async () => [
+    {item: '🥛', available: true},
+    {item: '🍌', available: false},
+  ]);
+  ProductClient.mockImplementation(() => {
+    return {
+      fetchItems
+    }
+  });
+
+  let productService;
+  beforeEach(() => {
+    productService = new ProductService();
+  });
+
+  it('should filter out only available items', async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(items.length).toBe(1);
+    expect(items).toEqual([{item: '🥛', available: true}]);
+  });
+
+  it('test', async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(fetchItems).toHaveBeenCalledTimes(1);
+  })
+});
